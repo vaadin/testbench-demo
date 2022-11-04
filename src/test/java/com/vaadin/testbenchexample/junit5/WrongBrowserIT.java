96d464ae794c51df423b78f6e712758ba2535d51
marcin <106965834+MarcinVaadin@users.noreply.github.com>
2022-11-04 14:00:11 +0100

    update after review

--- src/test/java/com/vaadin/testbenchexample/junit5/WrongBrowserIT.java
+++ src/test/java/com/vaadin/testbenchexample/junit5/WrongBrowserIT.java
@@ -1,6 +1,7 @@
 package com.vaadin.testbenchexample.junit5;
 
 import org.junit.jupiter.api.Assumptions;
+import org.openqa.selenium.Capabilities;
 
 import com.vaadin.testbench.BrowserTest;
 import com.vaadin.testbench.annotations.RunLocally;
@@ -14,9 +15,9 @@ import com.vaadin.testbench.parallel.BrowserUtil;
 @RunLocally(Browser.CHROME) // the driver (could be multi-browser remote driver - Sauce)
 public class WrongBrowserIT extends AbstractJUnit5IT {
 
-    @BrowserTest // that test will be ignored
-    public void clickButton_notificationShown_safari() {
-        Assumptions.assumeTrue(BrowserUtil.isSafari(getCapabilities()),
+    @BrowserTest // capabilities are injected by TestBench
+    public void clickButton_notificationShown_safari(Capabilities capabilities) {
+        Assumptions.assumeTrue(BrowserUtil.isSafari(capabilities),  // that test will be ignored
                 "Safari test only, will not run on: " + getCapabilities().getBrowserName());
         calculate_resultEquals("2", "+", "2", "4.0");
     }
